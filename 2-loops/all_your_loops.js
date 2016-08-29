0da3842637074a7e2b5cf4916f48ed85746d7617
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-29 15:03:06 -0500

    2-loops complete

--- 2-loops/all_your_loops.js
+++ 2-loops/all_your_loops.js
@@ -3,6 +3,33 @@
 // of checkz by 1
 var checkz = 0;
 
+for (var i = 0; i < 5; i++) {
+  checkz++;
+}
+
 // 2 - Write a while loop that runs
 // 3 times. Each iteration will decrease
 // the value of checkz by -2
+
+// Note from Luis - now I'm just being a "stickler", but technically these
+// instructions say to "decrease" by "-2"... not "decrease by 2", so I'm going
+// to be literal about it... sorry!
+
+var checkpoint = checkz;
+var goal = checkz-(-2*3);
+
+while (checkz<goal) {
+  checkz -= -2;
+}
+
+console.log('The value of checkz after a literal interpretation:', checkz);
+
+// And if you want to see it the other way...
+checkz = checkpoint;
+goal = checkz+(-2*3);
+
+while (checkz>goal) {
+  checkz -= 2;
+}
+
+console.log('The value of checkz after a generous interpretation:', checkz);
